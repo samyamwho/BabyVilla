09b9b6cee623ffcacba2fd44d332721ec04abe89
samyam <samyam233@gmail.com>
2025-12-12 15:41:40 +0545

    i have made the changesthat willl prolly work

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import ApplyPage from './pages/ApplyPage';
